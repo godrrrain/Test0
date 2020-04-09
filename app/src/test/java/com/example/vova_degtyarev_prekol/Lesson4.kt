783d8c0646ef522108b49d5c36e262df6338cb06
Vova <vova.degtyarev.01@mail.ru>
2020-04-09 18:25:03 +0500

    add Cart Presenter. Lesson 6's.

--- app/src/test/java/com/example/vova_degtyarev_prekol/Lesson4.kt
+++ app/src/test/java/com/example/vova_degtyarev_prekol/Lesson4.kt
@@ -9,30 +9,36 @@ import org.junit.Test
  */
 class Lesson4 {
 
+    private val cartPresenter = CartPresenter()
     @Test
     fun example() {
-
-        val iphoneCase = Product(price = 100.0, salePercent = 30)
-        val samsungCase = Product(price = 160.0, salePercent = 40)
-
-        val pricePrinter: PricePrinter = ProductionPrintPrice()
-
-        val products = listOf(iphoneCase, samsungCase)
-        val discountPrices = products.map { it.calcDiscountPrice() }
-
-        discountPrices.forEach { discountPrice ->
-            pricePrinter.print(discountPrice)
-        }
-
-        val cartMain = Cart(products)
-        pricePrinter.print(cartMain.calcFinalPrice())
-
+        cartPresenter.cartFullPrint()
     }
+}
+
+class CartPresenter {
+    private val iphoneCase = Product(price = 130.0, salePercent = 30, productName = "iPhone Case")
+    private val samsungCase = Product(price = 160.0, salePercent = 40, productName = "Samsung Case")
+    private val nokiaCase = Product(price = 100.0, salePercent = 10, productName = "Nokia Case")
+
+    private val pricePrinter: PricePrinter = ProductionPrintPrice()
+
+    private val cartMain = Cart(listOf(iphoneCase, samsungCase, nokiaCase))
+
+    fun cartFullPrint() {
+        cartMain.getList().forEach { product ->
+            pricePrinter.print("${product.getProductName()}: ${product.calcDiscountPrice()}")
+        }
+        pricePrinter.print(cartMain.calcFinalPrice())
+    }
+
 }
 
 class Cart(
     private val listOfProducts: List<Product> = emptyList()
 ) {
+    fun getList() = listOfProducts
+
     fun calcFinalPrice(): Double {
         var sum = 0.0
         listOfProducts.forEach {
@@ -43,12 +49,41 @@ class Cart(
 
 }
 
+class ProductPresenter {
+    private val iphoneCase = Product(price = 130.0, salePercent = 30, productName = "iPhone Case")
+    private val samsungCase = Product(price = 160.0, salePercent = 40, productName = "Samsung Case")
+
+    private val pricePrinter: PricePrinter = ProductionPrintPrice()
+
+    private val products = listOf(iphoneCase, samsungCase)
+
+    fun pricePrint() {
+        products.forEach { product ->
+            pricePrinter.print(product.calcDiscountPrice())
+        }
+    }
+
+    fun productNamePrint() {
+        products.forEach { product ->
+            pricePrinter.print(product.getProductName())
+        }
+    }
+
+    fun productNameWithPricePrint() {
+        products.forEach { product ->
+            pricePrinter.print("${product.getProductName()}: ${product.calcDiscountPrice()}")
+        }
+    }
+
+}
+
 class Product(
     /**
      * Must be positive
      */
     private val price: Double,
-    private val salePercent: Int = 0
+    private val salePercent: Int = 0,
+    private val productName: String = "unknown"
 ) {
     /**
      * @return price with applied discount determined by [salePercent]
@@ -57,6 +92,8 @@ class Product(
      * If [salePercent] less than 0 product price will be increased
      */
     fun calcDiscountPrice(): Double = price * (1 - salePercent / 100.0)
+
+    fun getProductName(): String = productName
 }
 
 interface PricePrinter {
@@ -67,6 +104,8 @@ interface PricePrinter {
      * If price have fractional part than it will be rounded for 2 symbols after "."
      */
     fun print(price: Double)
+
+    fun print(name: String)
 }
 
 class ProductionPrintPrice : PricePrinter {
@@ -78,6 +117,10 @@ class ProductionPrintPrice : PricePrinter {
             println(String.format("%.2f", price))
         }
 
+    }
+
+    override fun print(name: String) {
+        println(name)
     }
 
 }
